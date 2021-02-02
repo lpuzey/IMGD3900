91356f8811de606b9830b869253056a17ada4eb3
Linda Puzey <47217463+lpuzey@users.noreply.github.com>
2021-02-01 21:32:29 -0500

    Finished my Shazam game

--- A03/member1/game.js
+++ A03/member1/game.js
@@ -3,6 +3,27 @@
 /* jshint browser : true, devel : true, esversion : 6, freeze : true */
 /* globals PS : true */
 
+// Linda Puzey
+// Mod 1: I deleted many of the functions and variables that I didn't want (ex. sprites)
+// Mod 2: I added multiple different colors for the map
+// Mod 3: I completely reworked the map, with the new colors, to make Billy Batson
+// Mod 4: I created an entirely new map to create Shazam
+// Mod 5: I changed the gridcolor to gray
+// Mod 6: I reworked the touch function to switch between maps (Billy and Shazam)
+// Mod 7: I changed the status line to say "Click to SHAZAM!"
+// Mod 8: The touch funtion plays the fx_bang audio
+// Mod 9: I used a fader to flash the yellow color (symbolizing a lightning strike)
+// Mod 10: I made is so the gray beads also use the fader to flash yellow so that there isn't a box around Billy/Shazam
+// Mod 11: I changed the status line color to black
+// Mod 12: I added multiple audio files
+// Mod 13: I made it so he says "shazam" depending on which form he is in
+
+// AUDIO CREDITS
+//From the game LEGO DC Supervillians
+//All credit to Warner Bros. and Travelers Tales
+//SHAZAM belongs to DC comics
+
+
 "use strict";
 
 // The G object will contain all public constants, variables and functions.
@@ -16,168 +37,93 @@ const G = ( function () {
 	const WIDTH = 21; // grid width
 	const HEIGHT = 21; // grid height
 
-	const PLANE_FLOOR = 0; // z-plane of floor
-	const PLANE_ACTOR = 1; // z-plane of actor
+	const COLOR_BG = PS.COLOR_GRAY; // background color
+	const COLOR_OL = PS.COLOR_BLACK; // wall color
+	const COLOR_RED = PS.COLOR_RED; // floor color
+	const COLOR_YELLOW = PS.COLOR_YELLOW; // gold color
+	const COLOR_WHITE = PS.COLOR_WHITE; // white color
 
-	const COLOR_BG = PS.COLOR_GRAY_DARK; // background color
-	const COLOR_WALL = PS.COLOR_BLACK; // wall color
-	const COLOR_FLOOR = PS.COLOR_GRAY; // floor color
-	const COLOR_ACTOR = PS.COLOR_GREEN; // actor color
-	const COLOR_GOLD = PS.COLOR_YELLOW; // gold color
-	const COLOR_EXIT = PS.COLOR_BLUE; // exit color
+	const COLOR_JACKET = 0x956E3C; // brown color
+	const COLOR_PANTS = 0x2C375B; // dark blue color
+	const COLOR_GLOVES = 0x212728; // grey color
+	const COLOR_SOCKS = 0xABC6CB; // light grey color
+	const COLOR_HAIR = 0x503421; // brown color
 
-	const SOUND_FLOOR = "fx_click"; // touch floor sound
-	const SOUND_WALL = "fx_hoot"; // touch wall sound
-	const SOUND_GOLD = "fx_coin1"; // take coin sound
-	const SOUND_OPEN = "fx_powerup8"; // open exit sound
-	const SOUND_WIN = "fx_tada"; // win sound
-	const SOUND_ERROR = "fx_uhoh"; // error sound
+	const COLOR_SKIN = 0xFFEAB9; // tan color
 
-	const WALL = 0; // wall
-	const FLOOR = 1; // floor
-	const GOLD = 2; // floor + gold
+	const BACKGROUND = 0; // background
+	const OUTLINE = 1; // black
+	const REDSHIRT = 2; // red superhero shirt
+	const YELLOWSHIRT = 3; //yellow superhero shirt
+	const WHITESHIRT = 4; //White for the cape and eyes
 
-	// Variables
+	const JACKET = 5; //brown jacket
+	const PANTS = 6; //blue pants
+	const GLOVES = 7; //dark grey gloves
+	const SOCKS = 8; //light greay socks
+	const HAIR = 10; //brown hair
 
-	let id_sprite; // actor sprite id
-	let id_path; // pathmap id for pathfinder
-	let id_timer; // timer id
+	const SKIN = 9; // Skin Tone
 
-	let gold_count; // initial number of gold pieces in map
-	let gold_found; // gold pieces collected
-	let won = false; // true on win
 
-	// This handmade imageMap is used for map drawing and pathfinder logic
-	// All properties MUST be present!
-	// The map.data array controls the layout of the maze,
-	// the location of the gold pieces and exit
-	// 0 = wall, 1 = floor, 2 = floor + gold
-	// To remove a gold piece, replace a 2 with a 1
-	// To add a gold piece, replace a 1 with a 2
-
-	let map = {
+	//Billy Batson's map
+	let Billy = {
 		width: WIDTH, // must match WIDTH!
 		height: HEIGHT, // must match HEIGHT!
 		pixelSize: 1, // must be present!
 		data: [
 			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-			0, 1, 1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0,
-			0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0,
-			0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 2, 0,
-			0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 2, 1, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-			0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-			0, 1, 0, 0, 1, 2, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0,
-			0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 2, 0,
-			0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
-			0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 2, 0, 0, 1, 0, 0, 1, 0,
-			0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
-			0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0,
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+			0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 1, 10, 10, 10, 10, 10, 1, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 10, 10, 10, 10, 10, 10, 10, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 10, 9, 9, 9, 9, 9, 10, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 9, 9, 9, 9, 9, 9, 9, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 9, 9, 9, 9, 9, 9, 9, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 9, 1, 9, 9, 9, 1, 9, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 1, 1, 9, 4, 9, 9, 9, 4, 9, 1, 1, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 1, 2, 1, 1, 9, 9, 9, 9, 9, 1, 1, 2, 1, 0, 0, 0, 0,
+			0, 0, 0, 0, 1, 1, 5, 2, 1, 9, 9, 9, 1, 2, 5, 1, 1, 0, 0, 0, 0,
+			0, 0, 0, 0, 1, 5, 5, 1, 2, 1, 1, 1, 2, 1, 5, 5, 1, 0, 0, 0, 0,
+			0, 0, 0, 1, 5, 5, 1, 1, 5, 2, 6, 2, 5, 1, 1, 5, 5, 1, 0, 0, 0,
+			0, 0, 0, 1, 7, 7, 1, 1, 5, 5, 2, 5, 5, 1, 1, 7, 7, 1, 0, 0, 0,
+			0, 0, 0, 1, 7, 7, 1, 1, 5, 5, 5, 5, 5, 1, 1, 7, 7, 1, 0, 0, 0,
+			0, 0, 0, 0, 1, 1, 0, 1, 5, 5, 5, 5, 5, 1, 0, 1, 1, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 1, 6, 6, 1, 6, 6, 1, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 6, 6, 1, 0, 1, 6, 6, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 1, 1, 8, 8, 1, 0, 0, 0, 1, 8, 8, 1, 1, 0, 0, 0, 0,
+			0, 0, 0, 1, 4, 4, 4, 4, 1, 0, 0, 0, 1, 4, 4, 4, 4, 1, 0, 0, 0,
+			0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0
 		]
 	};
 
-	// These two variables control the initial location of the actor
-	// This location MUST correspond to a floor location (1) in the maza.data array
-	// or a startup error will occur!
-
-	let actorX = 1; // initial x-pos of actor sprite
-	let actorY = 1; // initial y-pos of actor sprite
-
-	// These two variables control the location of the exit
-	// This location MUST correspond to a floor location (1) in the maza.data array
-	// or a startup error will occur!
-
-	let exitX = 19; // x-pos of exit
-	let exitY = 19; // y-pos of exit
-	let exit_ready = false; // true when exit is opened
-
-	// Timer function, called every 1/10th sec
-	// This moves the actor along paths
-
-	let path; // path to follow, null if none
-	let step; // current step on path
-
-	const tick = function () {
-		let p, nx, ny, ptr, val;
-
-		if ( !path ) { // path invalid (null)?
-			return; // just exit
-		}
-
-		// Get next point on path
-
-		p = path[ step ];
-		nx = p[ 0 ]; // next x-pos
-		ny = p[ 1 ]; // next y-pos
-
-		// If actor already at next pos,
-		// path is exhausted, so nuke it
-
-		if ( ( actorX === nx ) && ( actorY === ny ) ) {
-			path = null;
-			return;
-		}
-
-		// Move sprite to next position
-
-		PS.spriteMove( id_sprite, nx, ny );
-		actorX = nx; // update actor's xpos
-		actorY = ny; // and ypos
-
-		// If actor has reached a gold piece, take it
-
-		ptr = ( actorY * HEIGHT ) + actorX; // pointer to map data under actor
-		val = map.data[ ptr ]; // get map data
-		if ( val === GOLD ) {
-			map.data[ ptr ] = FLOOR; // change gold to floor in map.data
-			PS.gridPlane( PLANE_FLOOR ); // switch to floor plane
-			PS.color( actorX, actorY, COLOR_FLOOR ); // change visible floor color
-
-			// If last gold has been collected, activate the exit
-
-			gold_found += 1; // update gold count
-			if ( gold_found >= gold_count ) {
-				exit_ready = true;
-				PS.color( exitX, exitY, COLOR_EXIT ); // show the exit
-				PS.glyphColor( exitX, exitY, PS.COLOR_WHITE ); // mark with white X
-				PS.glyph( exitX, exitY, "X" );
-				PS.statusText( "Found " + gold_found + " gold! Exit open!" );
-				PS.audioPlay( SOUND_OPEN );
-			}
-
-			// Otherwise just update score
-
-			else {
-				PS.statusText( "Found " + gold_found + " gold!" );
-				PS.audioPlay( SOUND_GOLD );
-			}
-		}
-
-		// If exit is ready and actor has reached it, end game
-
-		else if ( exit_ready && ( actorX === exitX ) && ( actorY === exitY ) ) {
-			PS.timerStop( id_timer ); // stop movement timer
-			PS.statusText( "You escaped with " + gold_found + " gold!" );
-			PS.audioPlay( SOUND_WIN );
-			won = true;
-			return;
-		}
-
-		step += 1; // point to next step
-
-		// If no more steps, nuke path
-
-		if ( step >= path.length ) {
-			path = null;
-		}
+	//Shazam's map
+	let Shazam = {
+		width: WIDTH, // must match WIDTH!
+		height: HEIGHT, // must match HEIGHT!
+		pixelSize: 1, // must be present!
+		data: [
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 1, 9, 9, 9, 9, 9, 1, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 9, 9, 9, 9, 9, 9, 9, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 9, 9, 9, 9, 9, 9, 9, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 9, 4, 9, 9, 9, 4, 9, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 9, 1, 9, 9, 9, 1, 9, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 1, 9, 9, 9, 9, 9, 1, 1, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 1, 1, 2, 2, 1, 9, 9, 9, 1, 2, 3, 1, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 1, 2, 2, 1, 3, 1, 1, 1, 3, 1, 4, 3, 1, 0, 0, 0, 0,
+			0, 0, 0, 1, 2, 2, 1, 1, 2, 2, 3, 3, 2, 1, 4, 4, 3, 1, 0, 0, 0,
+			0, 0, 0, 1, 3, 3, 1, 1, 2, 2, 3, 3, 2, 1, 4, 4, 3, 1, 0, 0, 0,
+			0, 0, 0, 1, 9, 9, 1, 1, 2, 2, 3, 2, 2, 1, 4, 4, 3, 1, 0, 0, 0,
+			0, 0, 0, 0, 1, 1, 0, 1, 3, 3, 3, 3, 3, 1, 4, 4, 3, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 2, 2, 1, 4, 4, 3, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 2, 2, 1, 0, 1, 2, 2, 1, 1, 3, 1, 0, 0, 0,
+			0, 0, 0, 0, 1, 1, 2, 2, 1, 0, 0, 0, 1, 2, 2, 1, 1, 0, 0, 0, 0,
+			0, 0, 0, 1, 3, 3, 3, 3, 1, 0, 0, 0, 1, 3, 3, 3, 3, 1, 0, 0, 0,
+			0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0
+		]
 	};
 
 	// Public functions are exposed in the global G object, which is initialized here.
@@ -196,106 +142,134 @@ const G = ( function () {
 
 			PS.gridSize( WIDTH, HEIGHT );
 
-			// Check for illegal actor/exit locations
-
-			val = map.data[ ( actorY * HEIGHT ) + actorX ]; // get map data under actor
-			if ( val !== FLOOR ) {
-				PS.debug( "ERROR: Actor not on empty floor!" );
-				PS.audioPlay( SOUND_ERROR );
-				return;
-			}
-
-			val = map.data[ ( exitY * HEIGHT ) + exitX ]; // get map data at exit position
-			if ( val !== FLOOR ) {
-				PS.debug( "ERROR: Exit not on empty floor!" );
-				PS.audioPlay( SOUND_ERROR );
-				return;
-			}
-
-			PS.gridColor( COLOR_BG ); // grid background color
+			PS.gridColor( PS.COLOR_GRAY); // grid background color
 			PS.border( PS.ALL, PS.ALL, 0 ); // no bead borders
-			PS.statusColor( PS.COLOR_WHITE );
-			PS.statusText( "Click/touch to move" );
+			PS.statusColor( PS.COLOR_BLACK ); //Statusline color
+			PS.statusText( "Click to SHAZAM!" ); //Statusline text
 
 			// Use the map.data array to draw the maze
 			// This also counts the number of gold pieces that have been placed
 
-			gold_count = gold_found = 0;
 			for ( y = 0; y < HEIGHT; y += 1 ) {
 				for ( x = 0; x < WIDTH; x += 1 ) {
-					val = map.data[ ( y * HEIGHT ) + x ]; // get data
-					if ( val === WALL ) {
-						color = COLOR_WALL;
+					val = Billy.data[ ( y * HEIGHT ) + x ]; // get data
+					if ( val === BACKGROUND ) {
+						color = COLOR_BG;
 					}
-					else if ( val === FLOOR ) {
-						color = COLOR_FLOOR;
+					else if ( val === OUTLINE ) {
+						color = COLOR_OL;
 					}
-					else if ( val === GOLD ) {
-						color = COLOR_GOLD;
-						gold_count += 1; // add to count
+					else if ( val === REDSHIRT ) {
+						color = COLOR_RED;
+					}
+					else if ( val === YELLOWSHIRT ) {
+						color = COLOR_YELLOW;
+					}
+					else if ( val === WHITESHIRT ) {
+						color = COLOR_WHITE;
+					}
+					else if ( val === SKIN ) {
+						color = COLOR_SKIN;
+					}
+					else if ( val === JACKET ) {
+						color = COLOR_JACKET;
+					}
+					else if ( val === PANTS ) {
+						color = COLOR_PANTS;
+					}
+					else if ( val === GLOVES ) {
+						color = COLOR_GLOVES;
+					}
+					else if ( val === SOCKS ) {
+						color = COLOR_SOCKS;
+					}
+					else if ( val === HAIR ) {
+						color = COLOR_HAIR;
 					}
 					PS.color( x, y, color );
 				}
 			}
 
-			// Preload & lock sounds
+			PS.audioLoad( "fx_bang" );
 
-			PS.audioLoad( SOUND_FLOOR, { lock : true } );
-			PS.audioLoad( SOUND_WALL, { lock : true } );
-			PS.audioLoad( SOUND_GOLD, { lock : true } );
-			PS.audioLoad( SOUND_OPEN, { lock : true } );
-			PS.audioLoad( SOUND_WIN, { lock : true } );
-
-			// Create 1x1 solid sprite for actor
-			// Place on actor plane in initial actor position
-
-			id_sprite = PS.spriteSolid( 1, 1 );
-			PS.spriteSolidColor( id_sprite, COLOR_ACTOR );
-			PS.spritePlane( id_sprite, PLANE_ACTOR );
-			PS.spriteMove( id_sprite, actorX, actorY );
-
-			// Create pathmap from our imageMap
-			// for use by pathfinder
-
-			id_path = PS.pathMap( map );
-
-			// Start the timer function that moves the actor
-			// Run at 10 frames/sec (every 6 ticks)
-
-			path = null; // start with no path
-			step = 0;
-			id_timer = PS.timerStart( 6, tick );
 		},
+		touch : function () {
+			//Setting vars for the func
+			let x, y, val, color, nextMap;
 
-		// touch( x, y )
-		// Set up new path for the actor to follow.
-		// NOTE: data and options parameters are currently unused.
+			//Sets the map to Shazam, if it is Shazam then it changes it to Billy
+			nextMap= Shazam;
+			if(PS.color( 10, 12 ) == COLOR_YELLOW){
+				nextMap = Billy;
+				PS.audioPlay( "Shazam", { path : "audio/"} );
 
-		touch : function ( x, y ) {
-			let line;
-
-			// Do nothing if game over
-
-			if ( won ) {
-				return;
+			}else{
+				PS.audioPlay( "BillyBatson", { path : "audio/"} );
 			}
 
-			// Use pathfinder to calculate a line from current actor position
-			// to touched position
+			//The thunder sound
+			PS.audioPlay( "fx_bang" );
 
-			line = PS.pathFind( id_path, actorX, actorY, x, y );
+			//The lightning flash
+			PS.gridColor( PS.COLOR_GRAY );
+			PS.gridFade( 5 );
+			PS.gridColor( PS.COLOR_YELLOW );
+			PS.gridFade( 5 );
+			PS.gridColor( PS.COLOR_GRAY );
 
-			// If line is not empty, it's valid,
-			// so make it the new path
-			// Otherwise hoot at the player
 
-			if ( line.length > 0 ) {
-				path = line;
-				step = 0; // start at beginning
-				PS.audioPlay( SOUND_FLOOR );
-			}
-			else {
-				PS.audioPlay( SOUND_WALL );
+
+			//A for loop that goes through and changes the colors to that of the map
+			for ( y = 0; y < HEIGHT; y += 1 ) {
+				for ( x = 0; x < WIDTH; x += 1 ) {
+					val = nextMap.data[ ( y * HEIGHT ) + x ]; // get data
+					if ( val === BACKGROUND ) {
+						color = COLOR_BG;
+						PS.fade(x,y, 5);
+					}
+					else if ( val === OUTLINE ) {
+						color = COLOR_OL;
+					}
+					else if ( val === REDSHIRT ) {
+						color = COLOR_RED;
+					}
+					else if ( val === YELLOWSHIRT ) {
+						color = COLOR_YELLOW;
+					}
+					else if ( val === WHITESHIRT ) {
+						color = COLOR_WHITE;
+					}
+					else if ( val === SKIN ) {
+						color = COLOR_SKIN;
+					}
+					else if ( val === JACKET ) {
+						color = COLOR_JACKET;
+					}
+					else if ( val === PANTS ) {
+						color = COLOR_PANTS;
+					}
+					else if ( val === GLOVES ) {
+						color = COLOR_GLOVES;
+					}
+					else if ( val === SOCKS ) {
+						color = COLOR_SOCKS;
+					}
+					else if ( val === HAIR ) {
+						color = COLOR_HAIR;
+					}
+
+					//Uses the fader of the background beads
+					if(color == COLOR_BG){
+
+						PS.color( x, y, color );
+						PS.fade(x,y, 5);
+						PS.color( x, y, COLOR_YELLOW);
+						PS.fade(x,y, 5);
+						PS.color( x, y, color);
+					}
+					//Changes the colors to those above
+					PS.color( x, y, color );
+				}
 			}
 		}
 	};
@@ -314,7 +288,7 @@ PS.init = function ( system, options ) {
 // Called when the mouse button is clicked on a bead, or when a bead is touched
 
 PS.touch = function ( x, y, data, options ) {
-	G.touch( x, y ); // initiates actor movement
+	G.touch(); // Does a bunch of stuff when it is clicked
 };
 
 
